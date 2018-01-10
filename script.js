c268f7d8184897d69bd4b12d93c1be85ba9397ea
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-01-10 16:14:44 +0100

    Add projects section

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+/*Scripts for section header*/
 var navigation = $("#nav");
 var navigationItem = $(navigation.find("li"));
 
 navigationItem.click(function(){
     var activeItem = navigation.find("li.current");
+    activeItem.removeClass();
+    $(this).addClass("current");
+});
+
+/*Scripts for section projects*/
+var navigationProjects = $("#projectsTabs");
+var navigationProjectsItem = $(navigationProjects.find("li"));
+
+navigationProjectsItem.click(function(){
+    var activeItem = navigationProjects.find("li.current");
     activeItem.removeClass();
     $(this).addClass("current");
 });
